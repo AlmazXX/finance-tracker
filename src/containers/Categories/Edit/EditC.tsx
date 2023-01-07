5be3a88c8bf7b93c2b819c946d6dc9ec65e0c7bf
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 19:34:36 +0600

    containers: Created EditC

---
+++ src/containers/Categories/Edit/EditC.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import CategoryForm from "../../../components/CategoryForm/CategoryForm";
+import Spinner from "../../../components/Spinner/Spinner";
+import {
+  selectOneCategory,
+  selectOneCategoryReceived,
+} from "../../../store/categSlice";
+import { editCategory, fetchOneCategory } from "../../../store/categThunk";
+import { Category } from "../../../types";
+
+const EditC = () => {
+  const dispatch = useAppDispatch();
+  const { id } = useParams() as { id: string };
+  const navigate = useNavigate();
+  const existingCategory = useAppSelector(selectOneCategory);
+  const categoryReceived = useAppSelector(selectOneCategoryReceived);
+
+  useEffect(() => {
+    dispatch(fetchOneCategory(id));
+  }, [id, dispatch]);
+
+  const onSubmit = async (category: Category) => {
+    await dispatch(editCategory({ id, category }));
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <h2>Edit category</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          {categoryReceived === "pending" ? (
+            <Spinner />
+          ) : existingCategory ? (
+            <CategoryForm onSubmit={onSubmit} existingCategory={existingCategory} />
+          ) : (
+            <p>Category not found</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditC;
