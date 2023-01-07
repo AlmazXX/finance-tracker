296a64ea32f3bab7ee41a2c9a2fa2c6afcacc800
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 15:21:04 +0600

    categThunk: Created fetchCategories

--- src/store/categThunk.ts
+++ src/store/categThunk.ts
@@ -1,10 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosApi } from "../axiosApi";
-import { Category } from "../types";
+import { ApiCategory, CategoriesList, Category } from "../types";
 
 export const createCategory = createAsyncThunk(
   "category/create",
   async (category: Category) => {
     await axiosApi.post("/categories.json", category);
+  }
+);
+
+export const fetchCategories = createAsyncThunk<ApiCategory[]>(
+  "category/fetch",
+  async () => {
+    const { data } = await axiosApi.get<CategoriesList>("/categories.json");
+    return data ? Object.keys(data).map((id) => ({ ...data[id], id })) : [];
   }
 );
