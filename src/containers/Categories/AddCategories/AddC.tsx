0491d3d33de394cf6ba4a8ac4f240a8b85e124fe
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 14:33:57 +0600

    containers: Created AddC

---
+++ src/containers/Categories/AddCategories/AddC.tsx
@@ -0,0 +1,16 @@
+const AddC = () => {
+  return (
+    <>
+      <div className="row mt-3">
+        <div className="col">
+          <h2>Create category</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+    </>
+  );
+};
+
+export default AddC;
