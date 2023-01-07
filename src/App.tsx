e6e3de33616b2bbf7d1007f0079976accd636291
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 19:35:06 +0600

    App: Added Route to EditC

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AddC from "./containers/Categories/Add/AddC";
 import Categories from "./containers/Categories/Categories";
 import Home from "./containers/Home/Home";
 import EditT from "./containers/Transactions/Edit/EditT";
+import EditC from "./containers/Categories/Edit/EditC";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/edit-transaction/:id" element={<EditT />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/create-category" element={<AddC />} />
+        <Route path="/edit-category/:id" element={<EditC />} />
       </Routes>
     </Layout>
   );
