8b9c4253e25296eca17fa1cbd80c709ea159de6b
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 21:56:07 +0600

    Transactions: Added total

--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { currency } from "../../constants";
 import {
   selectTransactions,
   selectTransactionsReceived,
@@ -17,16 +18,39 @@ const Transactions = () => {
     dispatch(fetchTransactions());
   }, [dispatch]);
 
+  const total = transactions.reduce(
+    (acc, transaction) =>
+      transaction.type === "income"
+        ? acc + transaction.amount
+        : acc - transaction.amount,
+    0
+  );
+
   return (
-    <div className="d-flex flex-column gap-3">
-      {transactionsReceived === "pending" ? (
-        <Spinner />
-      ) : (
-        transactions.map((transaction) => (
-          <TransactionItem key={transaction.id} transaction={transaction} />
-        ))
-      )}
-    </div>
+    <>
+      <div
+        className="p-2 border border-1 rounded-2 text-center mb-3 fs-3"
+        style={{ maxWidth: "300px" }}
+      >
+        <p className="m-0">
+          Total:{" "}
+          <strong className={total < 0 ? "text-danger" : "text-success"}>
+            {total} {currency}
+          </strong>
+        </p>
+      </div>
+      <div className="d-flex flex-column gap-3">
+        {transactionsReceived === "pending" ? (
+          <Spinner />
+        ) : transactions.length ? (
+          transactions.map((transaction) => (
+            <TransactionItem key={transaction.id} transaction={transaction} />
+          ))
+        ) : (
+          <p>No transactions yet</p>
+        )}
+      </div>
+    </>
   );
 };
 
