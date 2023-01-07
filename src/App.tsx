8f686201b9b5163ae3fc1e1aa284b9cf470c7a60
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 11:30:36 +0600

    App: Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <Layout>
+      <Routes>
+      </Routes>
     </Layout>
   );
 }
