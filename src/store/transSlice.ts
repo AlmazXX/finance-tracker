37418a30021bcc0737173c284977f6f7751ebd1b
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 13:17:18 +0600

    transSlice: Added createTransaction thunk

--- src/store/transSlice.ts
+++ src/store/transSlice.ts
@@ -1,19 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
+import { createTransaction } from "./transThunk";
 
 interface transState {
   oneIsSubmitted: "idle" | "pending" | "success" | "failure";
 }
 
 const initialState: transState = {
-  oneIsSubmitted: 'idle',
-}
+  oneIsSubmitted: "idle",
+};
 
 export const transSlice = createSlice({
-  name: 'transaction',
+  name: "transaction",
   initialState,
   reducers: {},
-})
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTransaction.pending, (state) => {
+        state.oneIsSubmitted = "pending";
+      })
+      .addCase(createTransaction.fulfilled, (state) => {
+        state.oneIsSubmitted = "success";
+      })
+      .addCase(createTransaction.rejected, (state) => {
+        state.oneIsSubmitted = "failure";
+      });
+  },
+});
 
 export const transactionReducer = transSlice.reducer;
-export const selectTransactionIsSubmitted = (state: RootState) => state.transaction.oneIsSubmitted;
+export const selectTransactionIsSubmitted = (state: RootState) =>
+  state.transaction.oneIsSubmitted;
