51a7f0c4af6837aec7fa850b06bee663a4ad52f4
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 14:34:50 +0600

    Navbar: Added link to AddC

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,25 +3,22 @@ import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar navbar-expand-sm navbar-dark bg-primary">
-      <div className="container justify-content-start">
+      <div className="container">
         <Link to="/" className="navbar-brand">
           Finance Tracker
         </Link>
-        <div className="d-flex flex-column flex-sm-row justify-content-between w-100">
+        <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <NavLink to="/categories" className="nav-link">
-                Categories
-              </NavLink>
+              <NavLink to="/categories" className="nav-link">Categories</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/create-transaction" className="nav-link">Add transaction</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/create-category" className="nav-link">Add category</NavLink>
             </li>
           </ul>
-          <Link
-            to="/create-transaction"
-            className="btn btn-outline-light shadow-lg"
-            style={{ width: "180px" }}
-          >
-            Add new transaction
-          </Link>
         </div>
       </div>
     </div>
