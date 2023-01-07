a5550e42a9e630d2face3c063ad20e7dafb80627
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 13:00:23 +0600

    AddT: Added closing of modal when leaving page

--- src/containers/AddTransaction/AddT.tsx
+++ src/containers/AddTransaction/AddT.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import Modal from '../../components/Modal/Modal';
 import TransactionForm from '../../components/TransactionForm/TransactionForm';
+import { close } from "../../store/modalSlice";
 
 const CreateT = () => {
+  const dispatch = useDispatch();
+  const {pathname} = useLocation();
+
+  useEffect(() => {
+    if (pathname !== '/') {
+      dispatch(close())
+    }
+  }, [pathname, dispatch])
+  
   return (
     <Modal title="Add Expense/Income">
       <TransactionForm/>
