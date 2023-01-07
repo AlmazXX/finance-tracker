afd1434281f82e0c8bbdadd41ab3a264ae65ab93
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 15:21:28 +0600

    categSlice: Added thunk fetchCategories

--- src/store/categSlice.ts
+++ src/store/categSlice.ts
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
-import { createCategory } from "./categThunk";
+import { ApiCategory } from "../types";
+import { createCategory, fetchCategories } from "./categThunk";
 
 interface CategState {
   oneIsSubmitted: "idle" | "pending" | "success" | "failure";
+  list: ApiCategory[];
+  listIsreceived: "idle" | "pending" | "success" | "failure";
 }
 
 const initialState: CategState = {
   oneIsSubmitted: "idle",
+  list: [],
+  listIsreceived: "idle",
 };
 
 export const categSlice = createSlice({
@@ -24,9 +29,22 @@ export const categSlice = createSlice({
       })
       .addCase(createCategory.rejected, (state) => {
         state.oneIsSubmitted = "failure";
+      })
+      .addCase(fetchCategories.pending, (state) => {
+        state.listIsreceived = "pending";
+      })
+      .addCase(fetchCategories.fulfilled, (state, { payload: list }) => {
+        state.listIsreceived = "success";
+        state.list = list;
+      })
+      .addCase(fetchCategories.rejected, (state) => {
+        state.listIsreceived = "failure";
       }),
 });
 
 export const categoryReducer = categSlice.reducer;
 export const selectCategoryIsSubmitted = (state: RootState) =>
   state.category.oneIsSubmitted;
+export const selectCategories = (state: RootState) => state.category.list;
+export const selectCategoriesReceived = (state: RootState) =>
+  state.category.listIsreceived;
