2415b143ad64d14c14da282b2ac3d0dd08150ed7
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 13:17:39 +0600

    AddT: Created onSubmit function

--- src/containers/AddTransaction/AddT.tsx
+++ src/containers/AddTransaction/AddT.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { useAppDispatch } from '../../app/hooks';
 import Modal from '../../components/Modal/Modal';
 import TransactionForm from '../../components/TransactionForm/TransactionForm';
 import { close } from "../../store/modalSlice";
+import { createTransaction } from '../../store/transThunk';
+import { Transaction } from '../../types';
 
 const CreateT = () => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const {pathname} = useLocation();
 
   useEffect(() => {
@@ -14,6 +16,11 @@ const CreateT = () => {
       dispatch(close())
     }
   }, [pathname, dispatch])
+
+  const onSubmit = async (transaction: Transaction) => {
+    await dispatch(createTransaction(transaction));
+    dispatch(close())
+  }
   
   return (
     <Modal title="Add Expense/Income">
