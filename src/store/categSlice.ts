684d04988d09c46fa583f81ef714d6143cfaad6e
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 15:03:28 +0600

    categSlice: Added createCategory thunk

--- src/store/categSlice.ts
+++ src/store/categSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
+import { createCategory } from "./categThunk";
 
 interface CategState {
   oneIsSubmitted: "idle" | "pending" | "success" | "failure";
@@ -13,6 +14,17 @@ export const categSlice = createSlice({
   name: "category",
   initialState,
   reducers: {},
+  extraReducers: (builder) =>
+    builder
+      .addCase(createCategory.pending, (state) => {
+        state.oneIsSubmitted = "pending";
+      })
+      .addCase(createCategory.fulfilled, (state) => {
+        state.oneIsSubmitted = "success";
+      })
+      .addCase(createCategory.rejected, (state) => {
+        state.oneIsSubmitted = "failure";
+      }),
 });
 
 export const categoryReducer = categSlice.reducer;
