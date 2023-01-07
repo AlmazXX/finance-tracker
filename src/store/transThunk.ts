b1e12cf5130d383450f778d1a36510a11e76fcdd
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 16:25:33 +0600

    transThunk: Created fetchTransactions

--- src/store/transThunk.ts
+++ src/store/transThunk.ts
@@ -1,10 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosApi } from "../axiosApi";
-import { Transaction } from "../types";
+import { ApiTransaction, Transaction, TransactionsList } from "../types";
 
 export const createTransaction = createAsyncThunk(
   "transaction/create",
   async (transaction: Transaction) => {
     await axiosApi.post("/transactions.json", transaction);
+  }
+);
+
+export const fetchTransactions = createAsyncThunk<ApiTransaction[]>(
+  "transaction/fetch",
+  async () => {
+    const { data } = await axiosApi.get<TransactionsList>("/transactions.json");
+    return data ? Object.keys(data).map((id) => ({ ...data[id], id })) : [];
   }
 );
