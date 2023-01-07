72ee5baee011bb391812f0dc2cab35129f4f10d3
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 17:14:34 +0600

    CategoryItem: Changed layout

--- src/components/CategoryItem/CategoryItem.tsx
+++ src/components/CategoryItem/CategoryItem.tsx
@@ -9,23 +9,31 @@ interface Props {
 
 const CategoryItem: FC<Props> = ({ category }) => {
   return (
-    <div className="card">
+    <div className="card" style={{maxWidth: '540px'}}>
       <div className="card-body">
-        <h5 className="card-title">{category.name}</h5>
-        <p
-          className={`card-text ${
-            category.type === "income" ? "text-success" : "text-danger"
-          }`}
-        >
-          {category.type}
-        </p>
-        <div className="d-flex gap-3">
-          <Link to={`edit/${category.id}`} className={`btn btn-primary `}>
-            {<BtnSpinner />}Edit
-          </Link>
-          <button className="btn btn-outline-danger">
-            {<BtnSpinner />}Delete
-          </button>
+        <div className="row align-items-center">
+          <div className="col col-sm-3">
+            <h5 className="card-title">{category.name}</h5>
+          </div>
+          <div className="col col-sm-3">
+            <p
+              className={`card-text ${
+                category.type === "income" ? "text-success" : "text-danger"
+              }`}
+            >
+              {category.type}
+            </p>
+          </div>
+          <div className="col-12 col-sm-6">
+            <div className="d-flex gap-3">
+              <Link to={`edit/${category.id}`} className={`btn btn-primary `}>
+                {<BtnSpinner />}Edit
+              </Link>
+              <button className="btn btn-outline-danger">
+                {<BtnSpinner />}Delete
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
