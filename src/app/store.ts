37674cf7bafc66a2d39c3e2a14f878787bc236f3
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 17:47:16 +0600

    project: Removed Modal

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { categoryReducer } from "../store/categSlice";
-import { modalReducer } from "../store/modalSlice";
 import { transactionReducer } from "../store/transSlice";
 
 export const store = configureStore({
   reducer: {
-    modal: modalReducer,
     transaction: transactionReducer,
     category: categoryReducer,
   },
