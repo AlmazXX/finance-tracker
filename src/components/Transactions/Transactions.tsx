787d5240636d4c016a72f4c390ea184a1e9fbe22
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 16:56:07 +0600

    components: Created Transactions

---
+++ src/components/Transactions/Transactions.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  selectTransactions,
+  selectTransactionsReceived,
+} from "../../store/transSlice";
+import { fetchTransactions } from "../../store/transThunk";
+import Spinner from "../Spinner/Spinner";
+import TransactionItem from "./TransactionItem";
+
+const Transactions = () => {
+  const dispatch = useAppDispatch();
+  const transactions = useAppSelector(selectTransactions);
+  const transactionsReceived = useAppSelector(selectTransactionsReceived);
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
+  return (
+    <div className="d-flex flex-column gap-3">
+      {transactionsReceived === "pending" ? (
+        <Spinner />
+      ) : (
+        transactions.map((transaction) => (
+          <TransactionItem transaction={transaction} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Transactions;
