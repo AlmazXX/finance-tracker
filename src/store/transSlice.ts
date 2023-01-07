47d763489f01be7e46a8921185c203f2e6230363
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 17:30:17 +0600

    transSlice: Added edit, fetchOne, delete thunks

--- src/store/transSlice.ts
+++ src/store/transSlice.ts
@@ -1,18 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
 import { ApiTransaction } from "../types";
-import { createTransaction, fetchTransactions } from "./transThunk";
+import {
+  createTransaction,
+  deleteTransaction,
+  editTransaction,
+  fetchOneTransaction,
+  fetchTransactions,
+} from "./transThunk";
 
 interface transState {
   oneIsSubmitted: "idle" | "pending" | "success" | "failure";
   list: ApiTransaction[];
   listReceived: "idle" | "pending" | "success" | "failure";
+  one: ApiTransaction | null;
+  oneReceived: "idle" | "pending" | "success" | "failure";
+  oneDeleted: string | false;
 }
 
 const initialState: transState = {
   oneIsSubmitted: "idle",
   list: [],
   listReceived: "idle",
+  one: null,
+  oneReceived: "idle",
+  oneDeleted: false,
 };
 
 export const transSlice = createSlice({
@@ -39,6 +51,37 @@ export const transSlice = createSlice({
       })
       .addCase(fetchTransactions.rejected, (state) => {
         state.listReceived = "failure";
+      })
+      .addCase(fetchOneTransaction.pending, (state) => {
+        state.oneReceived = "pending";
+      })
+      .addCase(
+        fetchOneTransaction.fulfilled,
+        (state, { payload: transaction }) => {
+          state.oneReceived = "success";
+          state.one = transaction;
+        }
+      )
+      .addCase(fetchOneTransaction.rejected, (state) => {
+        state.oneReceived = "failure";
+      })
+      .addCase(editTransaction.pending, (state) => {
+        state.oneIsSubmitted = "pending";
+      })
+      .addCase(editTransaction.fulfilled, (state) => {
+        state.oneIsSubmitted = "success";
+      })
+      .addCase(editTransaction.rejected, (state) => {
+        state.oneIsSubmitted = "failure";
+      })
+      .addCase(deleteTransaction.pending, (state, { meta }) => {
+        state.oneDeleted = meta.arg;
+      })
+      .addCase(deleteTransaction.fulfilled, (state) => {
+        state.oneDeleted = false;
+      })
+      .addCase(deleteTransaction.rejected, (state) => {
+        state.oneDeleted = false;
       });
   },
 });
@@ -49,3 +92,8 @@ export const selectTransactionIsSubmitted = (state: RootState) =>
 export const selectTransactions = (state: RootState) => state.transaction.list;
 export const selectTransactionsReceived = (state: RootState) =>
   state.transaction.listReceived;
+export const selectOneTransaction = (state: RootState) => state.transaction.one;
+export const selectOneTransactionReceived = (state: RootState) =>
+  state.transaction.oneReceived;
+export const selectOneTransactionDeleted = (state: RootState) =>
+  state.transaction.oneDeleted;
