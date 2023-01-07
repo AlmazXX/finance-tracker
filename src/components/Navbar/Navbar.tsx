3d0f527e0bb9a531847a6acc903288e307c53831
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 14:21:58 +0600

    Navbar: Added links to categories, create-trans

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar navbar-expand-sm navbar-dark bg-primary">
-      <div className="container">
+      <div className="container justify-content-start">
         <Link to="/" className="navbar-brand">
           Finance Tracker
         </Link>
+        <div className="d-flex flex-column flex-sm-row justify-content-between w-100">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink to="/categories" className="nav-link">
+                Categories
+              </NavLink>
+            </li>
+          </ul>
+          <Link
+            to="/create-transaction"
+            className="btn btn-outline-light shadow-lg"
+            style={{ width: "180px" }}
+          >
+            Add new transaction
+          </Link>
+        </div>
       </div>
     </div>
   );
