80022e48a9da9e1952efcdc62bd90f03f83fb1ea
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 17:58:20 +0600

    containers: Created EditT

---
+++ src/containers/Transactions/Edit/EditT.tsx
@@ -0,0 +1,16 @@
+const EditT = () => {
+  return (
+    <>
+      <div className="row mt-3">
+        <div className="col">
+          <h2>Edit transaction</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+    </>
+  );
+};
+
+export default EditT;
