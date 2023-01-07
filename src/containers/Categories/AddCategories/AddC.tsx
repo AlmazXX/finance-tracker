8b181420f88a6987feacf83b2cfcfcd42b57fe3a
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 15:08:45 +0600

    AddC: Created OnSubmit function

--- src/containers/Categories/AddCategories/AddC.tsx
+++ src/containers/Categories/AddCategories/AddC.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../../app/hooks";
 import CategoryForm from "../../../components/CategoryForm/CategoryForm";
+import { createCategory } from "../../../store/categThunk";
 import { Category } from "../../../types";
 
 const AddC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const onSubmit = (category: Category) => {
-    
-  }
-  
+  const onSubmit = async (category: Category) => {
+    await dispatch(createCategory(category));
+    navigate("/categories");
+  };
+
   return (
     <>
       <div className="row mt-3">
@@ -15,7 +21,9 @@ const AddC = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col"><CategoryForm onSubmit={onSubmit}/></div>
+        <div className="col">
+          <CategoryForm onSubmit={onSubmit} />
+        </div>
       </div>
     </>
   );
