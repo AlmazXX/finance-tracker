f4b9dfa88023518103fd6073493ac4adf450017a
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 17:14:02 +0600

    Categories: Added CategoryItem

--- src/containers/Categories/Categories.tsx
+++ src/containers/Categories/Categories.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import CategoryItem from "../../components/CategoryItem/CategoryItem";
+import Spinner from "../../components/Spinner/Spinner";
+import {
+  selectCategories,
+  selectCategoriesReceived,
+} from "../../store/categSlice";
+import { fetchCategories } from "../../store/categThunk";
+
 const Categories = () => {
+  const dispatch = useAppDispatch();
+  const categories = useAppSelector(selectCategories);
+  const categoriesReceived = useAppSelector(selectCategoriesReceived);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
   return (
     <>
       <div className="row mt-3">
@@ -7,7 +25,15 @@ const Categories = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col"></div>
+        <div className="col">
+          <div className="d-flex flex-column gap-3">
+            {categoriesReceived === "pending" ? (
+              <Spinner />
+            ) : (
+              categories.map((category) => <CategoryItem category={category} />)
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
