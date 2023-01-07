30f424a479c09d60d6d470d361f5425b13cf87fa
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 11:31:52 +0600

    components: Created Spinner BtnSpinner

---
+++ src/components/Spinner/BtnSpinner.tsx
@@ -0,0 +1,10 @@
+const BtnSpinner = () => {
+  return (
+    <>
+      <span className="spinner-border spinner-border-sm me-2"></span>
+      <span className="visually-hidden">Loading...</span>
+    </>
+  );
+};
+
+export default BtnSpinner;
