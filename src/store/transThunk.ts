636886c575b44083d279872dbdd2590139904376
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 16:55:20 +0600

    transThunk: Updated fetchTransactions

--- src/store/transThunk.ts
+++ src/store/transThunk.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosApi } from "../axiosApi";
-import { ApiTransaction, Transaction, TransactionsList } from "../types";
+import { ApiTransaction, Transaction, TransactionsCategories } from "../types";
 
 export const createTransaction = createAsyncThunk(
   "transaction/create",
@@ -12,7 +12,18 @@ export const createTransaction = createAsyncThunk(
 export const fetchTransactions = createAsyncThunk<ApiTransaction[]>(
   "transaction/fetch",
   async () => {
-    const { data } = await axiosApi.get<TransactionsList>("/transactions.json");
-    return data ? Object.keys(data).map((id) => ({ ...data[id], id })) : [];
+    const { data } = await axiosApi.get<TransactionsCategories>(".json");
+    const transactions = data.transactions;
+    const categories = data.categories;
+    return data
+      ? Object.keys(transactions)
+          .map((id) => ({
+            ...transactions[id],
+            category: categories[transactions[id].category].name,
+            type: categories[transactions[id].category].type,
+            id,
+          }))
+          .sort((a, b) => (b.date > a.date ? 1 : -1))
+      : [];
   }
 );
