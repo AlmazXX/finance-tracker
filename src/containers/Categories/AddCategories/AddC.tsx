424a6b1ae09a731bcae1bced318ae53888a888a0
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-07 14:54:50 +0600

    AddC: Added CategoryForm

--- src/containers/Categories/AddCategories/AddC.tsx
+++ src/containers/Categories/AddCategories/AddC.tsx
@@ -1,4 +1,12 @@
+import CategoryForm from "../../../components/CategoryForm/CategoryForm";
+import { Category } from "../../../types";
+
 const AddC = () => {
+
+  const onSubmit = (category: Category) => {
+    
+  }
+  
   return (
     <>
       <div className="row mt-3">
@@ -7,7 +15,7 @@ const AddC = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col"></div>
+        <div className="col"><CategoryForm onSubmit={onSubmit}/></div>
       </div>
     </>
   );
